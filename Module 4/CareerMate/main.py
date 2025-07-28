bd6ad33d7f908c9114dd9f8f5d5754b94c908a16
Sabit Siraji <sabitsiraji@gmail.com>
2025-07-28 15:04:32 +0600

    Add environment configuration and core functionality for CareerMate project
    - Updated .gitignore to exclude environment files.
    - Modified Jupyter notebook kernel specification to use 'ai-env' and updated Python version.
    - Created environment.env file with API configuration for OpenAI.
    - Implemented main.py with asynchronous OpenAI client and user profile management.
    - Added empty readme.md and requirements.txt files for project documentation and dependencies.
    - Removed outdated code files related to code review, personal assistant, travel planner, and writing assistant functionalities.

---
+++ Module 4/CareerMate/main.py
@@ -0,0 +1,41 @@
+import os
+import json
+import asyncio
+from datetime import datetime
+from typing import List, Optional
+from dataclasses import dataclass
+from pydantic import BaseModel, Field
+from dotenv import load_dotenv
+from openai import AsyncOpenAI
+from agents import Agent, OpenAIChatCompletionsModel, Runner, function_tool, set_tracing_disabled, Runner   
+
+load_dotenv()
+
+BASE_URL = os.getenv("BASE_URL")
+API_KEY = os.getenv("API_KEY")
+MODEL_NAME = os.getenv("MODEL_NAME")
+
+if not BASE_URL or not API_KEY or not MODEL_NAME:
+    raise ValueError(
+        "Please set BASE_URL, API_KEY, and MODEL_NAME."
+    )
+    
+client = AsyncOpenAI(base_url=BASE_URL, api_key=API_KEY)
+set_tracing_disabled(disabled=True)
+
+
+class SkillGap(BaseModel):
+    skills
+
+class JobRecommender(BaseModel):
+
+class CourseRecommender(BaseModel):
+    
+class CareerMateAgent(Agent):
+    
+class UserProfile(BaseModel):
+    name: str
+    skills: List[str]  # List of user skills
+    experience_years: Optional[int] = None
+    education: Optional[str] = None
+
